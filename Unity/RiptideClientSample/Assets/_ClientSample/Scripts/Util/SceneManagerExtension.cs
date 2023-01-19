3f0a42386175cc19d50629de597e20e22053ee52
eiei114 <nowaka37564@gmail.com>
2023-01-19 16:45:37 +0900

    add プロジェクトAssets

---
+++ Unity/RiptideClientSample/Assets/_ClientSample/Scripts/Util/SceneManagerExtension.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine.SceneManagement;
+
+namespace _ClientSample.Scripts.Util
+{
+    public class SceneManagerExtension
+    {
+        //シーン遷移の拡張
+        public static void LoadScene(string sceneName)
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+        
+        public static void LoadScene(int sceneBuildIndex)
+        {
+            SceneManager.LoadScene(sceneBuildIndex);
+        }
+        
+        public static void LoadSceneAsync(string sceneName)
+        {
+            SceneManager.LoadSceneAsync(sceneName);
+        }
+        
+        public static void LoadSceneAsync(int sceneBuildIndex)
+        {
+            SceneManager.LoadSceneAsync(sceneBuildIndex);
+        }
+        
+        public static void LoadSceneAdditive(string sceneName)
+        {
+            SceneManager.LoadScene(sceneName, LoadSceneMode.Additive);
+        }
+        
+        
+        public static void LoadSceneAdditive(int sceneBuildIndex)
+        {
+            SceneManager.LoadScene(sceneBuildIndex, LoadSceneMode.Additive);
+        }
+        
+        public static void LoadSceneAdditiveAsync(string sceneName)
+        {
+            SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+        }
+    }
+}
---
+++ Unity/RiptideClientSample/Assets/_ClientSample/Scripts/View/StartView.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace _ClientSample.Scripts.View
+{
+    public class StartView : MonoBehaviour
+    {
+        [SerializeField] private InputField _inputField;
+        [SerializeField] private Button _connectButton;
+        
+        private readonly UnityEvent<string> _connectEvent = new();
+
+        public UnityEvent<string> OnConnectButtonClicked => _connectEvent;
+        
+        private void Awake()
+        {
+            _connectButton.onClick.AddListener(() => OnConnectButtonClicked?.Invoke(_inputField.text));
+        }
+    }
+}
